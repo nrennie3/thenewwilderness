8220b1254310ee7d3f1742994f985e9635ef2717
Nicholas Rennie <nrennie3@gmail.com>
2026-02-27 16:23:32 -0800

    initial site personalization

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The New Wilderness';
+export const SITE_DESCRIPTION = 'Navigating faith, family, and the age of artificial intelligence.';
